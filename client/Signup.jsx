53fe7c46878cb75c918401bbbd17851bc484a240
malleshgangula <rajpatelgangula2@gmail.com>
2026-06-19 15:14:32 +0530

    Create Signup.jsx

---
+++ client/Signup.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div>
+      <h2>Signup Page</h2>
+      <input placeholder="Username" />
+      <input placeholder="Email" />
+      <input placeholder="Password" />
+      <button>Register</button>
+    </div>
+  );
+}
+
+export default Signup;
