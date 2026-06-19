6bb665f0db9f1d3b708575f4662f0a11e845c025
malleshgangula <rajpatelgangula2@gmail.com>
2026-06-19 15:14:53 +0530

    Add ForgotPassword component for password reset

---
+++ client/ForgotPassword.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ForgotPassword() {
+  return (
+    <div>
+      <h2>Forgot Password</h2>
+      <input placeholder="Enter Email" />
+      <button>Reset Password</button>
+    </div>
+  );
+}
+
+export default ForgotPassword;
