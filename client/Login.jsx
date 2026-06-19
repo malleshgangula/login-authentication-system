6555d4bf271e7f3142871186f7d2bdc58181d5cf
malleshgangula <rajpatelgangula2@gmail.com>
2026-06-19 15:13:50 +0530

    Create Login.jsx

---
+++ client/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <input placeholder="Email" />
+      <input placeholder="Password" />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
